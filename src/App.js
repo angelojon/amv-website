087fbd664f56fa7716760ff3bdd39ad25e863214
Angelo <macaldo.jonangelo@gmail.com>
2024-03-27 20:52:06 +0800

    pushing with different branch

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="container mx-auto">
       <h1 className="font-bold text-center mt-8">Welcome to My Events Website</h1>
-      <p className="font-bold text-center text-gray-700 mt-4">Discover upcoming events...</p>
+      <p className="font-bold text-center text-gray-700 mt-4">This is a test commit</p>
     </div>
   );
 }
